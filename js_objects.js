9d40977b3ed9d0258ac2d1eaec1b5d530140170c
Danielle H Gamble <dg12181@srcs.loc>
2018-01-22 11:09:45 -0800

    sayHi function first attempt

--- js_objects.js
+++ js_objects.js
@@ -1,10 +1,22 @@
-function Cube(length,mass) {
+function Cube(length, mass) {
     this.length = length;
     this.mass = mass;
-    this.volume = (Math.pow(this.length,3));
-    this.density = this.mass/this.volume;
-    this.surfaceArea = 6 * (Math.pow(this.length,2));
+    this.volume = (Math.pow(this.length, 3));
+    this.density = this.mass / this.volume;
+    this.surfaceArea = 6 * (Math.pow(this.length, 2));
 }
-function Person(firstName,lastName,birthDate){
 
+function Person(firstName, lastName, birthDate) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
+    this.sayHi = function () {
+        if (day = 0) {
+            return "sunday"
+        } else {
+            return "not sunday"
+        }
+
+    }
+}
 }
