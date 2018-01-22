9bd105408948507c5e8f425e228e0a3bfd75b112
Danielle H Gamble <dg12181@srcs.loc>
2018-01-22 11:43:10 -0800

    sayHi modified-90% complete

--- js_objects.js
+++ js_objects.js
@@ -6,17 +6,40 @@ function Cube(length, mass) {
     this.surfaceArea = 6 * (Math.pow(this.length, 2));
 }
 
+function sayHi() {
+     var today = new Date();
+            var day = today.getDay();
+            var hour = today.getHours();
+            if (hour >= 0 && hour <= 9) {
+                if (day == 0 || day == 6) {
+                    return "Go back to sleep, its the weekend!";
+                } else if (day == 1) {
+                    return "Ugh, its monday morning!";
+                } else {
+                    return "Good Morning!";
+                }
+            } else if (hour >= 10 && hour <= 17) {
+                if (day == 0 || day == 6) {
+                    return "Hope you are having a great weekend!";
+                } else {
+                    return "Good Day!";
+                }
+            } else {
+                if (day == 0) {
+                    return "Enjoy your evening...tomorrow is Monday!";
+                } else {
+                    return "Good Evening!";
+                }
+            }
+
+
+}
+
+function
+
 function Person(firstName, lastName, birthDate) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthDate = birthDate;
-    this.sayHi = function () {
-        if (day = 0) {
-            return "sunday"
-        } else {
-            return "not sunday"
-        }
-
-    }
-}
+    this.sayHi = sayHi;
 }
