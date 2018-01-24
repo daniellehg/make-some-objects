206fcf032bd39b7413138a8bd68da3dc958d7b16
Danielle H Gamble <dg12181@srcs.loc>
2018-01-24 12:17:42 -0800

    person complete except for zodiac + started pet function

--- js_objects.js
+++ js_objects.js
@@ -7,30 +7,30 @@ function Cube(length, mass) {
 }
 
 function sayHi() {
-     var today = new Date();
-            var day = today.getDay();
-            var hour = today.getHours();
-            if (hour >= 0 && hour <= 9) {
-                if (day == 0 || day == 6) {
-                    return "Go back to sleep, its the weekend!";
-                } else if (day == 1) {
-                    return "Ugh, its monday morning!";
-                } else {
-                    return "Good Morning!";
-                }
-            } else if (hour >= 10 && hour <= 17) {
-                if (day == 0 || day == 6) {
-                    return "Hope you are having a great weekend!";
-                } else {
-                    return "Good Day!";
-                }
-            } else {
-                if (day == 0) {
-                    return "Enjoy your evening...tomorrow is Monday!";
-                } else {
-                    return "Good Evening!";
-                }
-            }
+    var today = new Date();
+    var day = today.getDay();
+    var hour = today.getHours();
+    if (hour >= 0 && hour <= 9) {
+        if (day == 0 || day == 6) {
+            return "Go back to sleep, its the weekend!";
+        } else if (day == 1) {
+            return "Ugh, its monday morning!";
+        } else {
+            return "Good Morning!";
+        }
+    } else if (hour >= 10 && hour <= 17) {
+        if (day == 0 || day == 6) {
+            return "Hope you are having a great weekend!";
+        } else {
+            return "Good Day!";
+        }
+    } else {
+        if (day == 0) {
+            return "Enjoy your evening...tomorrow is Monday!";
+        } else {
+            return "Good Evening!";
+        }
+    }
 
 
 }
@@ -40,9 +40,27 @@ function Person(firstName, lastName, birthDate) {
     this.lastName = lastName;
     this.birthDate = new Date("03/09/2001");
     this.sayHi = sayHi;
-    this.age = function age(){
-    var date = new date;
-    return Math.floor((new Date() - this.birthDate)/1000/60/60/24/365);
+    this.age = function age() {
+        var date = new date;
+        return Math.floor((new Date() - this.birthDate) / 1000 / 60 / 60 / 24 / 365);
 
+    } this.getSign = function zodiac(){
+        var today = new Date();
+        var date = today.getDate();
+        var month = today.getMonth();
+        var bMonth = birthDate.bMonth();
+        var bDate = birthDate. bDate();
+        if(bMonth)
+    }
 }
+
+function Pet(name,species){
+    this.name = name;
+    this.species = species;
+    this.sleepy = 0;
+    this.hunger = 0;
+    this.awake = true;
+    this.speak = function speak(){
+
+    }
 }
